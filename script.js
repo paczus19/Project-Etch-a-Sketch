3a44c20cd969158a9757358b5061bf66c8665086
Robert Pączek <paczus19@aol.com>
2024-04-04 12:45:42 +0200

    Add function randomColor that randomize color of pixels each button click

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ inputButton.addEventListener("click", pickGrid) ;
 //function that can let user pick a resoultion in pixels
 function pickGrid() {
 let dimentions = prompt("Choose the resolution (1 - 100)");
-if(dimentions>100) { //check if dimentions are not to big
+if(dimentions>100 || dimentions<=0){ //check if dimentions are not to big
     pickGrid(); }
 else {
 const sketcher = document.querySelector(".sketcher");
-const onePixel = document.querySelector(".onePixel");
-
 //clear previous grid 
 while(sketcher.hasChildNodes()) {
 sketcher.removeChild(sketcher.firstChild); }
@@ -25,19 +23,26 @@ createGird(dimentions); } }
 
 //function that create gird with user dimentions
 function createGird(dimentions) {
+    //random color of pixels
+    let x = Math.floor(Math.random()*256);
+    let y = Math.floor(Math.random()*256);
+    let z = Math.floor(Math.random()*256);
+    let randomColor = `rgb(${x},${y},${z})`;
+
     const sketcher = document.querySelector(".sketcher");
     for(let i=0; i<dimentions*dimentions; i++) {
     const onePixel = document.createElement("div");
     onePixel.classList.add("onePixel");
+    onePixel.style.backgroundColor = "white";
     onePixel.style.width = 800/dimentions + "px";
     onePixel.style.height = 600/dimentions + "px";
     sketcher.appendChild(onePixel);
 
     //event, if mouse is over a div it changes color
     onePixel.onmouseover = function() {
-        onePixel.style.backgroundColor = "green";
+        onePixel.style.backgroundColor = randomColor;
+        onePixel.style.opacity = "90%";
     };
-    
     };  
 }
 
